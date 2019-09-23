35c8915e044ad663d2e9a88083e190ee1ed0fb72
saurabh1024 <55682954+saurabh1024@users.noreply.github.com>
2019-09-23 12:04:10 +0530

    Add files via upload

---
+++ new-offer.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NewOfferPage } from './new-offer.page';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NewOfferPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [NewOfferPage]
+})
+export class NewOfferPageModule {}
